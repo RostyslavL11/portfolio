87dd47cd4d9c716f3a1ee2341f47994ea99b3281
Rostyslav <RostikL1994@gmail.com>
2021-03-22 23:20:14 +0200

    section (portfolio) and added  (pulse / uber projects)

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__progress-item-number'),
+    lines = document.querySelectorAll('.skills__progress-item-body div');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
